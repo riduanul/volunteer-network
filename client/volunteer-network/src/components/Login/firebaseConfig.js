190517a7df825ead3a4254cd2d3b5d4f959ba047
riduanul <67514628+riduanul@users.noreply.github.com>
2020-10-09 04:08:10 +0600

    first commit

---
+++ client/volunteer-network/src/components/Login/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBQWMScxZ1PiZi8YkDekBXtSFM3Auc_g_M",
+    authDomain: "volunteer-network-new.firebaseapp.com",
+    databaseURL: "https://volunteer-network-new.firebaseio.com",
+    projectId: "volunteer-network-new",
+    storageBucket: "volunteer-network-new.appspot.com",
+    messagingSenderId: "497346658975",
+    appId: "1:497346658975:web:8573d27d99a9f9865a98aa"
+  };
+  export default firebaseConfig;
